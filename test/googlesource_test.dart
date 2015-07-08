8bf31b364e23ddc276f995ab69da48cb2248afb9
Ben Holtz <bholtz@cs.stanford.edu>
2015-07-07 17:11:33 -0700

    Adds googlesource library.
    This library allows for crawling the google source page and extracting the contents of files.

---
+++ test/googlesource_test.dart
@@ -0,0 +1,143 @@
+import 'dart:async';
+import 'dart:convert';
+
+import '../tool/googlesource.dart';
+import '../tool/simple_http_client.dart';
+import 'package:mock/mock.dart';
+import 'package:unittest/unittest.dart';
+
+void main() => defineTests();
+
+void defineTests() {
+  group('GoogleSourceFile', () {
+    GoogleSourceFile file;
+
+    void testHtmlConversion(List<String> lines) {
+      file = new GoogleSourceFile(asHtml(lines), 'example.com');
+
+      expect(file.fileContents, lines.join('\n'));
+    }
+
+    test('correctly parses simple raw html', () {
+      testHtmlConversion(['just one line']);
+    });
+
+    test('correctly parses multiline raw html', () {
+      testHtmlConversion(['multiple', 'small', 'lines']);
+    });
+
+    test('correctly parses multiline raw html with whitespace', () {
+      testHtmlConversion(['if (true)', '  goto label;', '', '', 'label']);
+    });
+
+    test('unescapes files', () {
+      var testEscapeString = 'this & that is <\"\'>';
+      var escapedHtmlFile = '<ol><li><a name="1"></a><span>'
+          '${HTML_ESCAPE.convert(testEscapeString)}</span></li></ol>';
+      file = new GoogleSourceFile(escapedHtmlFile, 'www.example.com');
+
+      expect(file.fileContents, testEscapeString);
+    });
+  });
+
+  group('GoogleSourceCrawler', () {
+    var baseUri = 'http://www.example.com/';
+    MockSimpleHttpClient client;
+    GoogleSourceCrawler crawler;
+
+    setUp(() {
+      client = new MockSimpleHttpClient();
+      crawler = new GoogleSourceCrawler(client, baseUri);
+
+      client.when(callsTo('getHtmlAtUri'))
+        ..thenReturn(new Future.value())
+        ..alwaysReturn(new Future.value(''));
+    });
+
+    test('returns correct files in single directory', () async {
+      prepopulateHttpResponses(client, test1);
+
+      var files = await crawler.findAllMatchingFiles('test').toList();
+
+      expect(files.length, 3);
+      expect(files[0].url, '/test/a.idl');
+      expect(files[1].url, '/test/b.idl');
+      expect(files[2].url, '/test/c.idl');
+    });
+
+    test('correctly follows the file tree', () async {
+      prepopulateHttpResponses(client, test2);
+
+      var files = await crawler.findAllMatchingFiles('test').toList();
+
+      expect(files.length, 1);
+      expect(files.single.url, '/test/foo/bar/baz/qux.idl');
+    });
+
+    test('rejects files with non-matching extensions', () async {
+      prepopulateHttpResponses(client, test3);
+
+      var files = await crawler.findAllMatchingFiles('test').toList();
+
+      expect(files.length, 2);
+      expect(files[0].url, '/test/a.idl');
+      expect(files[1].url, '/test/c.json');
+    });
+  });
+}
+
+void prepopulateHttpResponses(
+    MockSimpleHttpClient mockClient, List<String> responses) {
+  mockClient.reset();
+  for (var response in responses) {
+    mockClient
+        .when(callsTo('getHtmlAtUri'))
+        .thenReturn(new Future.value(response));
+  }
+}
+
+class MockSimpleHttpClient extends Mock implements SimpleHttpClient {
+  noSuchMethod(Invocation msg) => super.noSuchMethod(msg);
+}
+
+var test1 = [
+  '<ol>'
+      '<li><a href="/test/a.idl">a.idl</a></li>'
+      '<li><a href="/test/b.idl">b.idl</a></li>'
+      '<li><a href="/test/c.idl">c.idl</a></li>'
+      '</ol>',
+  'a contents',
+  'b contents',
+  'c contents'
+];
+
+var test2 = [
+  '<ol><li><a href="/test/foo/">foo</a></li></ol>',
+  '<ol><li><a href="/test/foo/bar/">bar</a></li></ol>',
+  '<ol><li><a href="/test/foo/bar/baz/">baz</a></li></ol>',
+  '<ol><li><a href="/test/foo/bar/baz/qux.idl">qux.idl</a></li></ol>',
+  'qux contents'
+];
+
+var test3 = [
+  '<ol><li><a href="/test/a.idl">a.idl</a></li>'
+      '<li><a href="/test/b.txt">b.txt</a></li>'
+      '<li><a href="/test/c.json">c.json</a></li></ol>',
+  'a contents',
+  'b contents',
+  'c contents'
+];
+
+String asHtml(List<String> lines) {
+  var liHtml = lines.map((line) => '<li><a></a><span>$line</span></li>').join();
+  return '<ol>$liHtml</ol>';
+}
+
+var multilineFileHtml = '''<ol>
+  <li><a name="1"></a><span>first line</span></li>
+  <li><a name="2"></a><span>  second line</span></li>
+  <li><a name="3"></a><span>just one line</span></li>
+  <li><a name="4"></a><span>just one line</span></li>
+  <li><a name="5"></a><span>just one line</span></li>
+  <li><a name="6"></a><span>just one line</span></li>
+</ol>''';
--- tool/googlesource.dart
+++ tool/googlesource.dart
@@ -1,6 +1,7 @@
 library googlesource;
 
 import 'dart:async';
+import 'dart:convert';
 
 import 'simple_http_client.dart';
 import 'tag_matcher.dart';
@@ -9,26 +10,38 @@ abstract class GoogleSourceEntity {
   final String _rawHtml;
   final String _url;
 
-  String get url => _url;
   GoogleSourceEntity(this._rawHtml, this._url);
+  String get url => _url;
+  String get _lines => TagMatcher.olMatcher.allContents(_rawHtml).single;
 }
 
 class GoogleSourceFile extends GoogleSourceEntity {
+  static const _encodedCharacters = const ['\'', '&', '<', '>', '\"'];
+  static final encodings =
+      new Map.fromIterable(_encodedCharacters, key: HTML_ESCAPE.convert);
+
   GoogleSourceFile(rawHtml, url) : super(rawHtml, url);
 
-  String get fileContents => TagMatcher.liMatcher
-      .allContents(_rawHtml)
+  String get fileContents => _unescapeHtml(TagMatcher.liMatcher
+      .allContents(_lines)
       .map((line) {
     var lineStripped = line.replaceFirst(TagMatcher.aMatcher, '');
     return TagMatcher.spanMatcher.allContents(lineStripped).join('');
-  }).join('\n');
+  }).join('\n'));
+
+  String _unescapeHtml(String escapedHtml) {
+    encodings.forEach((encodedString, decodedString) {
+      escapedHtml = escapedHtml.replaceAll(encodedString, decodedString);
+    });
+    return escapedHtml;
+  }
 }
 
 class GoogleSourceDirectory extends GoogleSourceEntity {
   GoogleSourceDirectory(rawHtml, url) : super(rawHtml, url);
 
-  Iterable<String> get listUris => TagMatcher.liMatcher
-      .allAttributes(_rawHtml)
+  Iterable<String> get listUris => TagMatcher.aMatcher
+      .allAttributes(_lines)
       .map((Map<String, String> attributes) => attributes['href']);
 }
 
@@ -41,12 +54,14 @@ class GoogleSourceCrawler {
   GoogleSourceCrawler(this._client, this._baseUri);
 
   Stream<GoogleSourceFile> findAllMatchingFiles(String relativeUri) async* {
-    var directory = new GoogleSourceDirectory(await _client.getHtmlAtUri(_absoluteUri(relativeUri)), relativeUri);
+    var directory = new GoogleSourceDirectory(
+        await _client.getHtmlAtUri(_absoluteUri(relativeUri)), relativeUri);
     for (var childUrl in directory.listUris) {
       if (childUrl.endsWith('/')) {
         yield* findAllMatchingFiles(childUrl);
       } else if (matchingExtensions.any((ext) => childUrl.endsWith(ext))) {
-        yield new GoogleSourceFile(await _client.getHtmlAtUri(_absoluteUri(relativeUri)), relativeUri);
+        yield new GoogleSourceFile(
+            await _client.getHtmlAtUri(_absoluteUri(childUrl)), childUrl);
       }
     }
   }
